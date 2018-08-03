c76e1a10ad15ee47da9fe17a52fb87a5cccaabe5
Vlad Frolov <frolvlad@gmail.com>
2018-08-03 15:18:14 +0300

    Added missing dependencies

--- lib/PrepareTool.sh
+++ lib/PrepareTool.sh
@@ -2,13 +2,24 @@
 
 set -e
 
-if ! hash apt-get 2>/dev/null; then
-    whiptail --title "Orangepi Build System" --msgbox "This scripts requires a Debian based distrbution. If you not use Debian/Ubunut, pls install:[ bsdtar mtools u-boot-tools pv bc sunxi-tools gcc automake make qemu dosfstools ]"
-    exit 1
-fi
-
-apt-get -y --no-install-recommends --fix-missing install \
+dependencies="\
+    whiptail \
+    curl git rsync unzip \
+    cpio figlet device-tree-compiler parted \
     bsdtar mtools u-boot-tools pv bc \
     gcc automake make \
     lib32z1 lib32z1-dev qemu-user-static \
-    dosfstools figlet device-tree-compiler debootstrap
+    dosfstools figlet device-tree-compiler debootstrap \
+"
+
+if ! hash apt-get 2>/dev/null; then
+    err_msg="This scripts requires a Debian based distrbution. If you not use Debian/Ubuntu, pls install:[ $dependencies ]"
+    if ! hash whiptail 2>/dev/null; then
+        echo "ERROR: $err_msg" >&2
+    else
+        whiptail --title "Orangepi Build System" --msgbox "$err_msg" 15 60 0
+    fi
+    exit 1
+fi
+
+apt-get -y --no-install-recommends --fix-missing install $dependencies
