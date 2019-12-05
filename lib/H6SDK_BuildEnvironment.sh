6c854ab3162c8d682f8fe1235f48874b80ca5bfa
leeboby <leeboby@aliyun.com>
2019-12-05 20:14:31 +0800

    Add mainline options for h6

--- lib/H6SDK_BuildEnvironment.sh
+++ lib/H6SDK_BuildEnvironment.sh
@@ -3,20 +3,26 @@
 KERNEL_VER=$(whiptail --title "Orange Pi Build System" \
         --menu "$MENUSTR" 10 40 3 --cancel-button Finish --ok-button Select \
 	"1"  "linux4.9" \
+	"2"  "mainline" \
         3>&1 1>&2 2>&3)
+
+SCRIPTS=("scripts" "master")
+EXTERNAL=("external" "master")
+TOOLCHAIN=("toolchain" "aarch64-linux-gnu-4.9")
 
 if [ "${KERNEL_VER}" = "0" ]; then
 	PLATFORM="OrangePiH6"
 	KERNEL=("OrangePiH6_kernel" "master")
 	UBOOT=("OrangePiH6_uboot" "master")
-else
+elif [ "${KERNEL_VER}" = "1"  ];then
 	PLATFORM="OrangePiH6_Linux4.9"
 	KERNEL=("OrangePiH6_Linux4_9" "master")
-	UBOOT=("OrangePiH6_uboot" "Linux4,9")
+	UBOOT=("OrangePiH6_uboot" "Linux4.9")
+elif [ "${KERNEL_VER}" = "2"  ];then
+	PLATFORM="OrangePiH6_mainline"
+	KERNEL=("mainline_kernel" "orangepi-h6")
+	UBOOT=("mainline_uboot" "orangepi-h6")
+	TOOLCHAIN=("toolchain" "aarch64-linux-gnu-7.4.1")
 fi
-
-SCRIPTS=("scripts" "master")
-EXTERNAL=("external" "master")
-TOOLCHAIN=("toolchain" "aarch64-linux-gnu-4.9")
 
 ORANGEPI_GIT="https://github.com/orangepi-xunlong"
