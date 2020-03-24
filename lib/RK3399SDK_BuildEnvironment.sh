56e47a98deda72d90fa6dac48016a437eba464f8
baiywt <baiywt_gj@163.com>
2020-03-24 17:04:46 +0800

    update lib/RK3399SDK_BuildEnvironment.sh

--- lib/RK3399SDK_BuildEnvironment.sh
+++ lib/RK3399SDK_BuildEnvironment.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-if [ "${BOARD}" = "18" ]; then
-	PLATFORM="OrangePiRK3399_Pi4"
-	KERNEL=("OrangePiRK3399_kernel" "master")
-	UBOOT=("OrangePiRK3399_uboot" "master")
-	SCRIPTS=("OrangePiRK3399_scripts" "orangepi_4")
-	EXTERNAL=("OrangePiRK3399_external" "orangepi_4")
-elif [ "${BOARD}" = "19" ]; then
-	PLATFORM="OrangePiRK3399"
-	KERNEL=("OrangePiRK3399_kernel" "linux4.4.103")
-	UBOOT=("OrangePiRK3399_uboot" "orangepi_rk3399")
-	SCRIPTS=("OrangePiRK3399_scripts" "master")
-	EXTERNAL=("OrangePiRK3399_external" "master")
-fi
+PLATFORM="OrangePiRK3399"
+KERNEL=("OrangePiRK3399_kernel" "master")
+UBOOT=("OrangePiRK3399_uboot" "master")
+SCRIPTS=("OrangePiRK3399_scripts" "orangepi-rk3399_v1.3")
+EXTERNAL=("OrangePiRK3399_external" "orangepi-rk3399_v1.3")
 
 TOOLCHAIN=("toolchain" "aarch64-linux-gnu-6.3")
 
