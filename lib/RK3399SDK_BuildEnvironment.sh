c3266fc489e712471278df43559b092e2dae7c4b
baiywt <baiywt_gj@163.com>
2020-09-28 20:49:43 +0800

    switch the scripts and external of rk3399 to orangepi-rk3399_v1.4

--- lib/RK3399SDK_BuildEnvironment.sh
+++ lib/RK3399SDK_BuildEnvironment.sh
@@ -3,8 +3,8 @@
 PLATFORM="OrangePiRK3399"
 KERNEL=("OrangePiRK3399_kernel" "master")
 UBOOT=("OrangePiRK3399_uboot" "master")
-SCRIPTS=("OrangePiRK3399_scripts" "orangepi-rk3399_v1.3")
-EXTERNAL=("OrangePiRK3399_external" "orangepi-rk3399_v1.3")
+SCRIPTS=("OrangePiRK3399_scripts" "orangepi-rk3399_v1.4")
+EXTERNAL=("OrangePiRK3399_external" "orangepi-rk3399_v1.4")
 
 TOOLCHAIN=("toolchain" "aarch64-linux-gnu-6.3")
 
