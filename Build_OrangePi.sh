f960fd74927c466628113fd749f85e7cda21df37
leeboby <leeboby@aliyun.com>
2021-03-18 10:19:41 +0800

    Remove some download options
     * H2 linux3.4
     * H3 linux3.4
     * Orange Pi 4/4b linux4.4
    
    The above has been moved to the latest compilation system:
    
      https://github.com/orangepi-xunlong/orangepi-build

--- Build_OrangePi.sh
+++ Build_OrangePi.sh
@@ -25,53 +25,36 @@ OPTION=$(whiptail --title "Orange Pi Build System" \
     3>&1 1>&2 2>&3)
 
 BOARD=$(whiptail --title "Orange Pi Build System" \
-	--menu "$MENUSTR" 32 60 23 --cancel-button Finish --ok-button Select \
-	"0"   "Orange Pi R1" \
-	"1"   "Orange Pi Zero" \
-	"2"   "Orange Pi One" \
-	"3"   "Orange Pi Lite" \
-	"4"   "Orange Pi PC" \
-	"5"   "Orange Pi PC Plus" \
-	"6"   "Orange Pi Plus" \
-	"7"   "Orange Pi Plus2E" \
-	"8"   "Orange Pi Zero Plus2(H3)" \
-	"9"   "Orange Pi PC2" \
-	"10"  "Orange Pi Prime" \
-	"11"  "Orange Pi Zero Plus" \
-	"12"  "Orange Pi Zero Plus2(H5)" \
-	"13"  "Orange Pi Win" \
-	"14"  "Orange Pi Win plus" \
-	"15"  "Orange Pi 4" \
-	"16"  "Orange Pi RK3399" \
-	"17"  "Orange Pi I96" \
-	"18"  "Orange Pi 2G-IOT" \
-	"19"  "Orange Pi 3G-iot" \
-	"20"  "Orange Pi 4G-iot" \
+	--menu "$MENUSTR" 20 60 10 --cancel-button Finish --ok-button Select \
+	"1"  "Orange Pi PC2" \
+	"2"  "Orange Pi Prime" \
+	"3"  "Orange Pi Zero Plus" \
+	"4"  "Orange Pi Zero Plus2(H5)" \
+	"5"  "Orange Pi Win/WinPlus" \
+	"6"  "Orange Pi RK3399" \
+	"7"  "Orange Pi I96" \
+	"8"  "Orange Pi 2G-IOT" \
+	"9"  "Orange Pi 3G-IOT" \
+	"10" "Orange Pi 4G-iot" \
 	3>&1 1>&2 2>&3)
 
 	case "${BOARD}" in
-		"0" | "1") #H2
-			source "${TOP_DIR}"/lib/H2SDK_BuildEnvironment.sh
-			;;
-		"2" | "3" | "4" | "5" | "6" | "7" | "8") #H3
-			source "${TOP_DIR}"/lib/H3SDK_BuildEnvironment.sh
-			;;
-		"9" | "10" | "11" | "12") #H5
+		"1" | "2" | "3" | "4") #H5
 			source "${TOP_DIR}"/lib/H5SDK_BuildEnvironment.sh
 			;;
-		"13" | "14") #A64
+		"5") #A64
 			source "${TOP_DIR}"/lib/A64SDK_BuildEnvironment.sh
 			;;
-		"15" | "16") #RK3399
+		"6") #RK3399
 			source "${TOP_DIR}"/lib/RK3399SDK_BuildEnvironment.sh
 			;;
-		"17" | "18") #RDA
+		"7" | "8") #RDA
 			source "${TOP_DIR}"/lib/RDASDK_BuildEnvironment.sh
 			;;
-		"19") #3G-IOT
+		"9") #3G-IOT
 			source "${TOP_DIR}"/lib/3G-iotSDK_BuildEnvironment.sh
 			;;
-		"20") #4G-IOT
+		"10") #4G-IOT
 			source "${TOP_DIR}"/lib/4G-iotSDK_BuildEnvironment.sh
 			;;
 		*) # Other
